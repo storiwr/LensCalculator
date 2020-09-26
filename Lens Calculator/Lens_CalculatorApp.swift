dd23e8283c50f5d8505794a4f1f60acde9a1e441
Storiwr <storiwr@users.noreply.github.com>
2020-09-26 10:31:30 -0400

    Initial Commit

---
+++ Lens Calculator/Lens_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lens_CalculatorApp.swift
+//  Lens Calculator
+//
+//  Created by Stephen on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Lens_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
